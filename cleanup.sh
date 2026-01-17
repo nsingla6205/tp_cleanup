fac7a3bda43fb238c236e6d374783e94c856e40f
nsingla <nsingla@netapp.com>
2026-01-17 09:37:50 +0530

    Scheduled

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@
 cd /Users/nsingla/GolandProjects/tp_cleanup
 
 # Run the cleanup script and log output
-/usr/local/go/bin/go run main.go >> /Users/nsingla/GolandProjects/tp_cleanup/cleanup.log 2>&1
+/opt/homebrew/bin/go run main.go >> /Users/nsingla/GolandProjects/tp_cleanup/cleanup.log 2>&1
 
 # Add timestamp to log
 echo "Cleanup completed at $(date)" >> /Users/nsingla/GolandProjects/tp_cleanup/cleanup.log
